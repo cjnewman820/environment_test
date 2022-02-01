eb4a5c98317f225fe21817119c286daace6b92ac
root <root@localhost>
2022-02-01 20:44:44 +0000

    Added unit and integration tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,9 +1,10 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
 
+# Book
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: "J K Rowling", price: 12, date: Date.today)
   end
 
   it 'is valid with valid attributes' do
@@ -11,6 +12,21 @@ RSpec.describe Book, type: :model do
   end
 
   it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a author' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
