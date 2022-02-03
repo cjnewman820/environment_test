5fa41f3034a50ffe8accb8766c2e32506006008e
root <root@localhost>
2022-02-03 17:38:39 +0000

    Completed book collection app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_154516) do
+ActiveRecord::Schema.define(version: 2022_02_02_182911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2022_01_21_154516) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
+    t.integer "price"
+    t.datetime "date"
   end
 
 end
